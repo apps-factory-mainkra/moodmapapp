6a95320077f14b1aab17e91e2658c3dd65d08e49
Aleix Falgosa Campoy <145427224+Aleix15@users.noreply.github.com>
2026-03-08 20:53:46 +0100

    chore: update generated app scaffold (moodmapapp)

---
+++ flutter_app/lib/utils/constants.dart
@@ -0,0 +1,11 @@
+// Constantes de la aplicación
+class AppConstants {
+  static const appName = '{spec.title}';
+  static const appVersion = '1.0.0';
+  
+  // Timeouts
+  static const defaultTimeout = Duration(seconds: 30);
+  
+  // Límites
+  static const maxRetries = 3;
+}
