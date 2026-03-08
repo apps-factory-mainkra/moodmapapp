d5e039882dc06fb78677468342803dc3446a7f42
Aleix Falgosa Campoy <145427224+Aleix15@users.noreply.github.com>
2026-03-08 20:53:08 +0100

    chore: update generated app scaffold (moodmapapp)

---
+++ flutter_app/lib/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {{
+  static const FirebaseOptions currentPlatform = FirebaseOptions(
+    apiKey: 'YOUR_API_KEY',
+    appId: 'YOUR_APP_ID',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    iosBundleId: 'com.example.app',
+    androidPackageName: 'com.example.app',
+  );
+}}
